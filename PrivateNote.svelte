9e8f6f88c1d8d7e5bf3cdf9e9cb57c3b1dae7ead
drewsokol <106027926+drewsokol@users.noreply.github.com>
2024-03-24 20:05:33 -0400

    Added basic setup and initial svelte PrivateNote component

---
+++ PrivateNote.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let content: string;
+
+  function handleInput(){
+
+  }
+
+  // Add your fetch, decrypt, encrypt, and save functions here
+</script>
+
+<div contenteditable="true" bind:textContent={content} on:input={handleInput}></div>
